662b35f40ebb2970961438ba2b4da5dc474a7247
HuyN2105 <huyn.dev.2105@gmail.com>
2024-08-23 20:19:33 +0700

    isMobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,12 @@
 import CursorFollower from './CursorFollower/CursorFollower.jsx';
 import MainPage from './Main/MainPage';
 import NavBar from './NavBar/NavBar';
+import { isMobile } from 'react-device-detect';
 
 function Home() {
 	return (
 		<>
-			<CursorFollower />
+			{isMobile ? <></> : <CursorFollower />}
 			<NavBar />
 			<MainPage />
 		</>
