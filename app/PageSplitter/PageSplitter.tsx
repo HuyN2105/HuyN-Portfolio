510194d4af5fc75aea1a05552994adf5d7f2eeb5
HuyN2105 <huyn.dev.2105@gmail.com>
2024-09-18 21:21:51 +0700

    PageSplitter cleanup

--- app/PageSplitter/PageSplitter.tsx
+++ app/PageSplitter/PageSplitter.tsx
@@ -5,6 +5,8 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 function PageSplitter() {
+	const PageSplitterBannerAmount = 7;
+
 	useLayoutEffect(() => {
 		let ctx = gsap.context(() => {
 			let tl = gsap.timeline({
@@ -39,92 +41,28 @@ function PageSplitter() {
 			className='rotate-90 w-[120vw] border-t border-b relative translate-x-[42vw] mix-blend-difference'
 		>
 			<div id='Row1' className='flex whitespace-nowrap'>
-				<div className='Name text-white pr-[2.5vh] font-medium text-[2.75vh]'>
-					HUYNGUYEN
-				</div>
-				<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[2.75vh]'>
-					FRONT-END DEVELOPER
-				</div>
-				<div className='Name text-white pr-[2.5vh] font-medium text-[2.75vh]'>
-					HUYNGUYEN
-				</div>
-				<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[2.75vh]'>
-					FRONT-END DEVELOPER
-				</div>
-				<div className='Name text-white pr-[2.5vh] font-medium text-[2.75vh]'>
-					HUYNGUYEN
-				</div>
-				<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[2.75vh]'>
-					FRONT-END DEVELOPER
-				</div>
-				<div className='Name text-white pr-[2.5vh] font-medium text-[2.75vh]'>
-					HUYNGUYEN
-				</div>
-				<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[2.75vh]'>
-					FRONT-END DEVELOPER
-				</div>
-				<div className='Name text-white pr-[2.5vh] font-medium text-[2.75vh]'>
-					HUYNGUYEN
-				</div>
-				<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[2.75vh]'>
-					FRONT-END DEVELOPER
-				</div>
-				<div className='Name text-white pr-[2.5vh] font-medium text-[2.75vh]'>
-					HUYNGUYEN
-				</div>
-				<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[2.75vh]'>
-					FRONT-END DEVELOPER
-				</div>
-				<div className='Name text-white pr-[2.5vh] font-medium text-[2.75vh]'>
-					HUYNGUYEN
-				</div>
-				<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[2.75vh]'>
-					FRONT-END DEVELOPER
-				</div>
+				{Array.from({ length: PageSplitterBannerAmount }).map((_) => (
+					<>
+						<div className='Name text-white pr-[2.5vh] font-medium text-[2.75vh]'>
+							HUYNGUYEN
+						</div>
+						<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[2.75vh]'>
+							FRONT-END DEVELOPER
+						</div>
+					</>
+				))}
 			</div>
 			<div id='Row2' className='flex whitespace-nowrap rotate-180 h-[2.5vh]'>
-				<div className='Name text-white pr-[2.5vh] font-medium text-[2.75vh]'>
-					HUYNGUYEN
-				</div>
-				<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[2.75vh]'>
-					FRONT-END DEVELOPER
-				</div>
-				<div className='Name text-white pr-[2.5vh] font-medium text-[2.75vh]'>
-					HUYNGUYEN
-				</div>
-				<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[2.75vh]'>
-					FRONT-END DEVELOPER
-				</div>
-				<div className='Name text-white pr-[2.5vh] font-medium text-[2.75vh]'>
-					HUYNGUYEN
-				</div>
-				<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[2.75vh]'>
-					FRONT-END DEVELOPER
-				</div>
-				<div className='Name text-white pr-[2.5vh] font-medium text-[2.75vh]'>
-					HUYNGUYEN
-				</div>
-				<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[2.75vh]'>
-					FRONT-END DEVELOPER
-				</div>
-				<div className='Name text-white pr-[2.5vh] font-medium text-[2.75vh]'>
-					HUYNGUYEN
-				</div>
-				<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[2.75vh]'>
-					FRONT-END DEVELOPER
-				</div>
-				<div className='Name text-white pr-[2.5vh] font-medium text-[2.75vh]'>
-					HUYNGUYEN
-				</div>
-				<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[2.75vh]'>
-					FRONT-END DEVELOPER
-				</div>
-				<div className='Name text-white pr-[2.5vh] font-medium text-[2.75vh]'>
-					HUYNGUYEN
-				</div>
-				<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[2.75vh]'>
-					FRONT-END DEVELOPER
-				</div>
+				{Array.from({ length: PageSplitterBannerAmount }).map((_) => (
+					<>
+						<div className='Name text-white pr-[2.5vh] font-medium text-[2.75vh]'>
+							HUYNGUYEN
+						</div>
+						<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[2.75vh]'>
+							FRONT-END DEVELOPER
+						</div>
+					</>
+				))}
 			</div>
 		</div>
 	);
