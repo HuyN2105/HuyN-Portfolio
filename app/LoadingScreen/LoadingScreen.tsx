59da19294f60d3c3d174beeb1e0ef7d8f1f64861
HuyN2105 <huyn.dev.2105@gmail.com>
2024-09-18 20:50:59 +0700

    Loading Screen UI

--- app/LoadingScreen/LoadingScreen.tsx
+++ app/LoadingScreen/LoadingScreen.tsx
@@ -1,30 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import gsap from 'gsap';
 
-interface LoadingScreenProps {
-	LoadingState: boolean;
-}
+import './LoadingScreen.css';
 
-function LoadingScreen({ LoadingState }: LoadingScreenProps) {
-	// Handler for the proxy
-	const variableHandler = {
-		set(target, property, value) {
-			console.log(
-				`Variable "${property}" changed from ${target[property]} to ${value}`
-			);
-			target[property] = value;
-			return true;
-		},
-	};
+function LoadingScreen() {
+	const [showSplash, setShowSplash] = useState(true);
 
-	// Proxy for the loading variable to monitor changes
-	const monitoredVariable = new Proxy({ value: LoadingState }, variableHandler);
+	useEffect(() => {
+		if (document.readyState !== 'complete') {
+			const handler = () => {
+				console.log('load');
+				setShowSplash(false);
+			};
+			window.addEventListener('load', handler);
+
+			return () => {
+				window.removeEventListener('load', handler);
+			};
+		} else {
+			const timeout = window.setTimeout(() => {
+				console.log('timeout');
+				setShowSplash(false);
+			}, 0);
+			return () => window.clearTimeout(timeout);
+		}
+	}, []);
+
+	useLayoutEffect(() => {
+		gsap.to('#LoadingText', {
+			opacity: 0,
+			duration: 0.4,
+			delay: 0.5,
+		});
+		gsap.to('#Loading1', {
+			x: '-100vw',
+			duration: 0.9,
+			ease: 'power1.out',
+			delay: 1.15,
+		});
+		gsap.to('#Loading2', {
+			x: '-100vw',
+			duration: 0.85,
+			ease: 'power1.inOut',
+			delay: 1.125,
+		});
+	}, [showSplash]);
 
 	return (
-		<div className='center fixed text-white cursor-default z-[9999] bg-[#0b0b0b] h-screen w-screen'>
-			<div className='fixed top-[50vh] left-[50vw] -translate-x-1/2 -translate-y-1/2'>
-				LoadingScreen
+		<>
+			<div
+				className='center fixed text-white cursor-default z-[9999] bg-[#0b0b0b] h-screen w-screen'
+				id='Loading1'
+			>
+				<div
+					className='fixed top-[50vh] left-[50vw] -translate-x-1/2 -translate-y-1/2'
+					id='LoadingText'
+				>
+					<div className='relative m-3 overflow-hidden tracking-widest font-normal text-xl rounded-3xl h-10 w-[25vw] cursor-default border flex text-nowrap border-white items-center whitespace-nowrap'>
+						<div
+							id='LoadingState1'
+							className='absolute -translate-x-full z-1000'
+						>
+							PLEASE WAIT, WEBSITE'S CONTENT IS BEING LOADED
+						</div>
+						<div id='LoadingState2' className='absolute -translate-x-full'>
+							PLEASE WAIT, WEBSITE'S CONTENT IS BEING LOADED
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
+			<div
+				className='w-screen h-screen bg-green-600 fixed z-[9900]'
+				id='Loading2'
+			></div>
+		</>
 	);
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -5,33 +5,9 @@ import HorizontalScroll from './HorizontalScroll/HorizontalScroll';
 import LoadingScreen from './LoadingScreen/LoadingScreen';
 
 function Home() {
-	const [showSplash, setShowSplash] = useState(true);
-	const [LoadingState, setLoadingState] = useState(true);
-
-	useEffect(() => {
-		if (document.readyState !== 'complete') {
-			const handler = () => {
-				console.log('load');
-				setShowSplash(false);
-			};
-			window.addEventListener('load', handler);
-
-			return () => {
-				window.removeEventListener('load', handler);
-			};
-		} else {
-			const timeout = window.setTimeout(() => {
-				console.log('timeout');
-				setShowSplash(false);
-				setLoadingState(false);
-			}, 0);
-			return () => window.clearTimeout(timeout);
-		}
-	}, []);
-
 	return (
 		<>
-			{showSplash ? <LoadingScreen LoadingState={LoadingState} /> : <></>}
+			<LoadingScreen />
 			<HorizontalScroll />
 		</>
 	);
