1fe1cd5b80cfbbdd9342fc81c876e16124a49971
HuyN2105 <huyn.dev.2105@gmail.com>
2024-09-20 14:42:47 +0700

    Mobile Device unsupported

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,29 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import HorizontalScroll from './HorizontalScroll/HorizontalScroll';
 import LoadingScreen from './LoadingScreen/LoadingScreen';
+import { isMobile } from 'react-device-detect';
+import ContactLinks from './ContactLinks/ContactLinks';
 
 function Home() {
 	return (
 		<>
-			<LoadingScreen />
-			<HorizontalScroll />
+			{isMobile ? (
+				<>
+					<div className='bg-#0b0b0b text-white text-[2.5vh] font-bold text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 '>
+						THIS WEBSITE IS CURRENTLY <p className='text-red-500'>NOT</p>
+						SUPPORT MOBILE DEVICES. ACCESS WITH COMPUTER OR COMEBACK IN THE NEAR
+						FUTURE.
+					</div>
+					<ContactLinks />
+				</>
+			) : (
+				<>
+					<LoadingScreen />
+					<HorizontalScroll />{' '}
+				</>
+			)}
 		</>
 	);
 }
