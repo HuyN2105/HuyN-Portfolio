58844f3d9376cf86f0a062a4df62520624b05431
HuyN2105 <huyn.dev.2105@gmail.com>
2024-10-04 10:57:09 +0700

    Refactor PageSplitter component to use unique IDs for triggers and elements

--- app/PageSplitter/PageSplitter.tsx
+++ app/PageSplitter/PageSplitter.tsx
@@ -13,22 +13,22 @@ function PageSplitter({ SplitterID }: PageSplitterProps) {
 
 	useLayoutEffect(() => {
 		let ctx = gsap.context(() => {
-			gsap.to('#Row2', {
+			gsap.to(`#Row2-${SplitterID}`, {
 				xPercent: 25,
 				ease: 'none',
 				scrollTrigger: {
-					trigger: '#PageSplitter',
+					trigger: `#PageSplitter-${SplitterID}`,
 					scrub: 1,
 					pinSpacing: true,
 					start: 'top top',
 					end: '+=600%',
 				},
 			});
-			gsap.to('#Row1', {
+			gsap.to(`#Row1-${SplitterID}`, {
 				xPercent: -25,
 				ease: 'none',
 				scrollTrigger: {
-					trigger: '#PageSplitter',
+					trigger: `#PageSplitter-${SplitterID}`,
 					scrub: 1,
 					pinSpacing: true,
 					start: 'top top',
@@ -41,15 +41,15 @@ function PageSplitter({ SplitterID }: PageSplitterProps) {
 
 	return (
 		<div
-			id='PageSplitter'
+			id={`PageSplitter-${SplitterID}`}
 			className='rotate-90 w-[120vw] border-t border-b relative translate-x-[42vw] mix-blend-difference'
 		>
 			{SplitterID === 1 ? (
 				<div>
-					<div id='Row1' className='flex whitespace-nowrap'>
+					<div id={`Row1-${SplitterID}`} className='flex whitespace-nowrap'>
 						{Array.from({ length: PageSplitterBannerAmount }).map(
 							(_, index) => (
-								<React.Fragment key={`row1-${index}`}>
+								<React.Fragment key={`row1-${SplitterID}-${index}`}>
 									<div className='Name text-white pr-[2.5vh] font-medium text-[3vh]'>
 										HUYNGUYEN
 									</div>
@@ -61,12 +61,12 @@ function PageSplitter({ SplitterID }: PageSplitterProps) {
 						)}
 					</div>
 					<div
-						id='Row2'
+						id={`Row2-${SplitterID}`}
 						className='flex whitespace-nowrap rotate-180 h-[2.5vh]'
 					>
 						{Array.from({ length: PageSplitterBannerAmount }).map(
 							(_, index) => (
-								<React.Fragment key={`row2-${index}`}>
+								<React.Fragment key={`row2-${SplitterID}-${index}`}>
 									<div className='Name text-white pr-[2.5vh] font-medium text-[3vh]'>
 										HUYNGUYEN
 									</div>
@@ -79,7 +79,21 @@ function PageSplitter({ SplitterID }: PageSplitterProps) {
 					</div>
 				</div>
 			) : (
-				<></>
+				<div
+					id={`Row2-${SplitterID}`}
+					className='flex whitespace-nowrap rotate-180 h-[2.5vh]'
+				>
+					{Array.from({ length: PageSplitterBannerAmount }).map((_, index) => (
+						<React.Fragment key={`row2-${SplitterID}-${index}`}>
+							<div className='Name text-white pr-[2.5vh] font-medium text-[3vh]'>
+								HUYNGUYEN
+							</div>
+							<div className='GreenText text-green-500 pr-[2.5vh] font-medium text-[3vh]'>
+								FULL-STACK DEVELOPER
+							</div>
+						</React.Fragment>
+					))}
+				</div>
 			)}
 		</div>
 	);
